cb62dd446644084936c8aba5094550fc0d044726
NM17 <nada.mahmoud@detroitlabs.com>
2019-03-04 11:58:15 -0500

    testing branch

--- src/main/java/com/detroitlabs/etaboggle/data/WordBankRepository.java
+++ src/main/java/com/detroitlabs/etaboggle/data/WordBankRepository.java
@@ -1,4 +1,14 @@
 package com.detroitlabs.etaboggle.data;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WordBankRepository {
+
+    private static final List<String> words = Arrays.asList(
+            new String("toobad")
+
+    );
 }
