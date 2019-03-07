b5a6bae0a11e11a3f2b2cd274c25a4c06f483a92
NM17 <nada.mahmoud@detroitlabs.com>
2019-03-06 21:13:25 -0500

    adding click feature to letters on board

--- build/resources/main/static/index.js
+++ build/resources/main/static/index.js
@@ -30,3 +30,107 @@ $('#js-resetTimer').click(function () {
     $('.js-timeout').text("2:00");
     clearInterval(interval);
 });
+
+$(function () {
+    $('.button').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'D');
+    });
+});
+
+$(function () {
+    $('.button2').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'L');
+    });
+});
+
+$(function () {
+    $('.button3').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'P');
+    });
+});
+$(function () {
+    $('.button4').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'A');
+    });
+});
+$(function () {
+    $('.button5').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'E');
+    });
+});
+$(function () {
+    $('.button6').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'I');
+    });
+});
+$(function () {
+    $('.button7').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'T');
+    });
+});
+$(function () {
+    $('.button8').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'G');
+    });
+});
+$(function () {
+    $('.button9').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'O');
+    });
+});
+
+$(function () {
+    $('.button10').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'N');
+    });
+});
+
+
+$(function () {
+    $('.button11').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'U');
+    });
+});
+
+$(function () {
+    $('.button12').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'N');
+    });
+});
+
+$(function () {
+    $('.button13').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'O');
+    });
+});
+$(function () {
+    $('.button14').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'Q');
+    });
+});
+$(function () {
+    $('.button15').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'E');
+    });
+});
+$(function () {
+    $('.button16').on('click', function () {
+        var text = $('.form2');
+        text.val(text.val() + 'U');
+    });
+});
